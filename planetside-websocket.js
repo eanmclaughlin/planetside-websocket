2c517f654f767e2e00ae470d4e0aea1c1dbef77e
Ean McLaughlin <computertechie2@gmail.com>
2015-10-20 19:36:57 -0600

    Expose underlying socket's send function. Add reconnect on socket close with delay in config.

--- planetside-websocket.js
+++ planetside-websocket.js
@@ -6,28 +6,40 @@ var EventEmitter = require('events');
 var util = require('util');
 var WebSocket = require('ws');
 
-function PS2Socket() {
+var PS2Socket = function () {
     EventEmitter.call(this);
     var self = this;
-    var socket = new WebSocket(config.url + config.service_id);
+    this.socket = {};
 
-    socket.on('open', function () {
-        self.emit('socket-open', new Date());
-    });
+    function connect() {
+        console.log("connect()");
+        self.socket = new WebSocket(config.socket_url + config.service_id);
 
-    socket.on('message', function (data) {
-        var dataObj = JSON.parse(data);
-        if (dataObj.service == "event" && dataObj.type == "serviceMessage") {
-            self.emit(dataObj.payload.event_name, dataObj.payload);
-        }
-    });
+        self.socket.on('open', function () {
+            self.emit('socket-open', new Date());
+        });
 
-    socket.on('close', function () {
-        self.emit('socket-close', new Date());
-    });
-}
+        self.socket.on('message', function (data) {
+            var dataObj = JSON.parse(data);
+            if (dataObj.service == "event" && dataObj.type == "serviceMessage") {
+                self.emit(dataObj.payload.event_name, dataObj.payload);
+            }
+        });
+
+        self.socket.on('close', function () {
+            self.emit('socket-close', new Date());
+            setTimeout(connect, config.reconnect_delay);
+        });
+    }
+
+    connect();
+};
 
 util.inherits(PS2Socket, EventEmitter);
+
+PS2Socket.prototype.send = function (message, options, callback) {
+    this.socket.send(message, options, callback);
+};
 
 module.exports = new PS2Socket();
 
